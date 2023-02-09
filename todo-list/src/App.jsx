44de2c4517d2c6b51cf96f2ae77dd1e87e2bd2c2
Angela Carvalho <angelalopo@outlook.com>
2023-02-09 10:50:03 -0300

    importa o componente todolist

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import TodoList from './TodoList'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
